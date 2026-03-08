258f3b532f9783a45315a4d8298f07bd6852336c
Prasanna <prasannag1505@gmail.com>
2026-03-09 00:35:14 +0530

    Home Controller Repath

--- src/main/java/com/example/Event_Management/Controller/HomeController.java
+++ src/main/java/com/example/Event_Management/Controller/HomeController.java
@@ -1,4 +1,4 @@
-package com.example.Event_Management;
+package com.example.Event_Management.Controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
